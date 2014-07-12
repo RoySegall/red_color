d093429e630ca2b0db71e34fdd0d3d8b35c8bd73
Roy Segall <roy.segall@gizra.com>
2014-07-12 22:01:51 +0300

    Check errors each 1 seconds.

--- assets/redColor.js
+++ assets/redColor.js
@@ -2,21 +2,27 @@
 
   Drupal.behaviors.RedColorAlerts = {
     attach: function (context) {
-      $.ajax({
-        url: 'red_color',
-        dataType: 'json',
-        type: 'get',
-        crossDomain: true,
-        contentType: "application/json; charset=utf-8",
-        success: function(msg) {
-          if (msg == null) {
-            return;
-          }
+      var messages = '';
+      setInterval(function() {
+        $.ajax({
+          url: 'red_color',
+          dataType: 'json',
+          type: 'get',
+          crossDomain: true,
+          contentType: "application/json; charset=utf-8",
+          success: function(msg) {
+            if (msg == 'NO_RESULTS') {
+              return;
+            }
 
-          // todo: remove later.
-          console.log(msg);
-        }
-      });
+            if (JSON.stringify(messages) !== JSON.stringify(msg)) {
+              console.log(msg);
+            }
+
+            messages = msg;
+          }
+        });
+      }, 1000);
     }
   };
 
